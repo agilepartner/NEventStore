d512716cbad57126ea8a3705e53096b4d9d74130
cpontet <cedric.pontet@gmail.com>
2013-08-09 14:50:46 +0200

    Implemented log4Net logger

---
+++ src/EventStore.Logging.Log4Net.CF/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EventStore.Logging.Log4Net")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("6f9acc09-b97d-49d9-9427-a1be10d77834")]
